5d091166626815741c3070bf1c226b99a6825781
Richard Zhang <rrzhang@usc.edu>
2023-01-14 10:44:05 -0800

    scroll scaffold eth deployment

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -27,17 +27,14 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   // Getting a previously deployed contract
   const YourContract = await ethers.getContract("YourContract", deployer);
-  /*  await YourContract.setPurpose("Hello");
-  
-    // To take ownership of yourContract using the ownable library uncomment next line and add the 
-    // address you want to be the owner. 
-    
-    await YourContract.transferOwnership(
-      "ADDRESS_HERE"
-    );
+  await YourContract.setPurpose("Hello");
 
-    //const YourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
+  // To take ownership of yourContract using the ownable library uncomment next line and add the
+  // address you want to be the owner.
+
+  await YourContract.transferOwnership("INSERT_PUBLIC_KEY");
+
+  // const YourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
 
   /*
   //If you want to send value to an address from the deployer
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -24,7 +24,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "scrollTestnet";
 
 function mnemonic() {
   try {
@@ -68,6 +68,12 @@ module.exports = {
 
       */
     },
+    scrollTestnet: {
+      url: "https://prealpha.scroll.io/l2" || "",
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
     mainnet: {
       url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
       //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/mainnet", // <---- YOUR MORALIS ID! (not limited to infura)
@@ -83,14 +89,14 @@ module.exports = {
       },
     },
     gnosis: {
-      url: 'https://rpc.gnosischain.com/',
+      url: "https://rpc.gnosischain.com/",
       gasPrice: 1000000000,
       accounts: {
         mnemonic: mnemonic(),
       },
     },
     chiado: {
-      url: 'https://rpc.chiadochain.net',
+      url: "https://rpc.chiadochain.net",
       gasPrice: 1000000000,
       accounts: {
         mnemonic: mnemonic(),
